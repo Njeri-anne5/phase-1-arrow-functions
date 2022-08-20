e462dcf0fef5c8aad3523282152f8c68a5d6c999
+ <+>
2022-08-20 07:38:10 -0400

    Done

---
+++ index.js
@@ -0,0 +1,14 @@
+
+//divide
+const divide=(c,d)=>(c/d);
+    
+    console.log(divide(2000,100));
+
+
+//square 
+const square=(x)=>(x*x);
+console.log(square(2));
+
+//add
+const add= (a,b)=>(a+b);
+console.log(add(3,4));
